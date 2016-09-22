a3182c1d008bf51d9c2531731909aa2f25507f4e
DeeKeilholz <deekeilholz@gmail.com>
2016-09-22 14:55:35 +0000

    Add LoopyLighthouse exercise

---
+++ loopyLighthouse.js
@@ -0,0 +1,18 @@
+// Program that prints numbers 100 to 200:
+for (var counter = 100; counter <= 200; counter++) {
+  if (counter % 3 === 0 && counter % 4 === 0)
+    console.log("LoopyLighthouse");
+  else if (counter % 3 === 0)
+    console.log("Loopy");
+  else if (counter % 4 === 0)
+    console.log("Lighthouse");
+  else
+    console.log(counter);
+}
+
+
+
+
+
+
+
